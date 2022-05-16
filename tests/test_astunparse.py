2ccd44f1400e007637561e0f56b3eef4500f792a
tristanlatr <tris.la.tr@gmail.com>
2022-05-15 20:53:47 -0400

    Refactor some code in inference.py into it's own module.

---
+++ tests/test_astunparse.py
@@ -0,0 +1,59 @@
+
+from astuce import _astunparse
+from . import AstuceTestCase
+
+class TestAstUnparseFunction(AstuceTestCase):
+
+    expressions = \
+        [
+            # operations
+            "b + c",
+            "b - c",
+            "b * c",
+            "b / c",
+            "b // c",
+            "b ** c",
+            "b ^ c",
+            "b & c",
+            "b | c",
+            "b @ c",
+            "b % c",
+            "b >> c",
+            "b << c",
+            # unary operations
+            "+b",
+            "-b",
+            "~b",
+            # comparisons
+            "b == c",
+            "b >= c",
+            "b > c",
+            "b <= c",
+            "b < c",
+            "b != c",
+            # boolean logic
+            "b and c",
+            "b or c",
+            "not b",
+            # identify
+            "b is c",
+            "b is not c",
+            # membership
+            "b in c",
+            "b not in c",
+        ]
+    
+    def test_building_value_from_nodes(self):
+        """Test building value from AST nodes."""
+        for expression in self.expressions:
+            module = self.parse(f"a = {expression}")
+            assert "a" in module.locals
+            value = module.locals["a"][0].statement.value
+            unparsed = value.unparse()
+
+            # Workaround the extra parenthesis added by the unparse() function.
+            if unparsed.startswith('(') and unparsed.endswith(')'):
+                unparsed = unparsed[1:-1]
+
+            assert unparsed == expression
+            assert _astunparse.unparse(value) == expression
