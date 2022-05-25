a4b89bb8dc53ab6a527e4286c8143fc7181a9912
tristanlatr <tris.la.tr@gmail.com>
2022-05-25 17:32:09 -0400

    Add a lookup test, again from astroid

--- tests/test_lookup.py
+++ tests/test_lookup.py
@@ -1,3 +1,6 @@
+import ast
+
+from astuce import nodes
 from .test_nodes import CODE_IF_BRANCHES_STATEMENTS
 from . import AstuceTestCase
 class LookupTest(AstuceTestCase):
@@ -37,3 +40,26 @@ class LookupTest(AstuceTestCase):
         
         # assert that the 'from os import path' statement is filtered.
         assert mod.lookup('path')[1] == [mod.body[1]]
+
+    def test_annassigned_stmts(self):
+        # assert that an empty annassign node name can't be resolved an returns Uninferable.
+        mod = self.parse("""
+        a: str = "abc"
+        b: str
+        """)
+        annassign_stmts = mod.body[0], mod.body[1]
+        simple_annassign_node = annassign_stmts[0].target
+        empty_annassign_node = annassign_stmts[1].target
+
+        assert simple_annassign_node == mod.lookup('a')[1]
+        
+        simple_inferred = list(simple_annassign_node.infer())
+        
+        self.assertEqual(1, len(simple_inferred))
+        self.assertIsInstance(simple_inferred[0], ast.Constant)
+        self.assertEqual(simple_inferred[0].value, "abc")
+
+        empty_annassign_inferred = list(empty_annassign_node.infer())
+
+        self.assertEqual(1, len(empty_annassign_inferred))
+        self.assertIs(empty_annassign_inferred[0], nodes.Uninferable)
